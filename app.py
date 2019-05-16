9bf243d86624eed65e3223288aac8794f4730100
rmbeard <rachel.beard@asu.edu>
2019-05-16 12:19:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-# Created by Rachel Beard: last updated 4/11/19
+# Created by Rachel Beard: last updated 1/11/19
 # Purpose: This is the entry point for a flask application that serves to
 # Zoovision, a spatial decision support web application
 import logging
 from flask import render_template,  request
-from map import maps1, sum_chart
+# from flask_bootstrap import Bootstrap
+# from flask_nav import Nav
+# from flask_nav.elements import Navbar, Subgroup, View, Link, Text
+# from flask_googlemaps import GoogleMaps
+# from wtforms import SelectField, SubmitField, Form, TextField, TextAreaField, validators
+# from flask_wtf import FlaskForm
+from map import maps1, maps2, sum_chart, local_moran_test
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import scoped_session, sessionmaker
 from flask import Flask, session
-
+# from db_setup import init_db
 
 app = Flask(__name__)
+# Bootstrap(app)
+# nav = Nav(app)
+
+# nav.register_element('my_navbar', Navbar('thenav', View('Home', 'home')))
 
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
@@ -22,6 +35,7 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 #                                          autoflush=False,
 #                                          bind=engine))
 # db = SQLAlchemy(app)
+
 # init_db()
 
 
@@ -34,39 +48,44 @@ def session_management():
 @app.route("/", methods=['GET', 'POST'])
 def home():
     file = "./data/Export_Output.shp"
-    seasons = ['2016-17', '2017-18', '2018-19']
+    # file = "C:\zoovision\data\Export_Output.shp"
+    # set default parameters
+    seasons = ['2015-16', '2016-17', '2017-18', '2018-19']
     states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas']
-    viruses = ['H1N1', 'H3N2']
-    risk_factors = ['PERCENT POSITIVE', '%UNWEIGHTED ILI']
+    weights = ['Distance', 'Genetic transition rate']
+    risk_factors = ["ILI POSITIVE", 'PERCENT POSITIVE', '%UNWEIGHTED ILI']
     week = ''
     if request.method == 'GET':
-        session['selected_risk'] = "PERCENT POSITIVE"
+        session['selected_risk'] = "ILI POSITIVE"
         session['selected_season'] = '2015-16'
         session['selected_week'] = 0
         session['selected_state'] = 'Alabama'
+        session['selected_weight'] = 'Distance'
         selected_risk = session['selected_risk']
         selected_season = session['selected_season']
         selected_week = session['selected_week']
         selected_state = session['selected_state']
+        selected_weight = session['selected_weight']
     elif request.method == 'POST':
         if 'Query' in request.form:
             selected_season = request.form['season']
-            # selected_virus = request.form['virus']
+            # selected_weight = request.form['weight']
             selected_risk = request.form['risk_factor']
             session['selected_risk'] = selected_risk
             session['selected_season'] = selected_season
+            # session['selected_weight'] = selected_weight
             selected_week = session['selected_week']
             selected_state = session['selected_state']
-            print(selected_week)
+            selected_weight = session['selected_weight']
+            print(selected_risk)
             if selected_week < 13:
                 selected_val = selected_week + 40
             else:
                 selected_val = selected_week - 12
             result = maps1(file, selected_risk, selected_season, selected_val)
             chart = sum_chart()
-            return render_template("tab.html", states=states, selected_state=selected_state, week=week, min=40, max=52, selected_week=selected_week, seasons=seasons,
-                                   selected_risk=selected_risk, selected_season=selected_season,
-                                   viruses=viruses, risk_factors=risk_factors, result=result, chart=chart)
+            return render_template("tab.html", weights=weights, selected_weight=selected_weight, states=states, selected_state=selected_state, week=week, min=40, max=52, selected_week=selected_week, seasons=seasons,
+                                   selected_risk=selected_risk, selected_season=selected_season, risk_factors=risk_factors, result=result, chart=chart)
         elif 'hello' in request.form:
             selected_week = request.form['week']
             selected_week = int(selected_week)
@@ -76,14 +95,15 @@ def home():
             else:
                 selected_val = selected_week - 12
             selected_risk = session['selected_risk']
+            selected_weight = session['selected_weight']
             selected_season = session['selected_season']
             selected_state = session['selected_state']
             session['selected_week'] = selected_week
             print(selected_week)
             result = maps1(file, selected_risk, selected_season, selected_val)
             chart = sum_chart()
-            return render_template("tab.html", states=states, selected_state=selected_state, selected_week=selected_week, week=week, min=40, max=52, seasons=seasons,
-                                   selected_risk=selected_risk, selected_season=selected_season, viruses=viruses, risk_factors=risk_factors,
+            return render_template("tab.html", weights=weights, selected_weight=selected_weight, states=states, selected_state=selected_state, selected_week=selected_week, week=week, min=40, max=52, seasons=seasons,
+                                   selected_risk=selected_risk, selected_season=selected_season, risk_factors=risk_factors,
                                    result=result, chart=chart)
         elif 'state' in request.form:
             selected_state = request.form['state']
@@ -95,14 +115,34 @@ def home():
             else:
                 selected_val = selected_week - 12
             selected_risk = session['selected_risk']
+            selected_weight = request.form['weight']
             selected_season = session['selected_season']
             session['selected_state'] = selected_state
             print(selected_week)
             result = maps1(file, selected_risk, selected_season, selected_val)
             chart = sum_chart()
-            return render_template("tab.html", states=states, selected_state=selected_state, selected_week=selected_week, week=week, min=40, max=52, seasons=seasons,
-                                   selected_risk=selected_risk, selected_season=selected_season, viruses=viruses, risk_factors=risk_factors,
+            return render_template("tab.html", weights=weights, selected_weight=selected_weight, states=states, selected_state=selected_state, selected_week=selected_week, week=week, min=40, max=52, seasons=seasons,
+                                   selected_risk=selected_risk, selected_season=selected_season, risk_factors=risk_factors,
                                    result=result, chart=chart)
+        elif 'case' in request.form:
+            selected_risk = request.form['risk_factor']
+            session['selected_risk'] = selected_risk
+            selected_weight = request.form['weight']
+            selected_week = session['selected_week']
+            if selected_week < 13:
+                selected_val = selected_week + 40
+            else:
+                selected_val = selected_week - 12
+            selected_state = session['selected_state']
+            selected_week = session['selected_week']
+            selected_season = session['selected_season']
+            print(selected_week)
+            result_cluster = local_moran_test(file, selected_risk, selected_season, selected_val, selected_weight, selected_week)
+            chart = sum_chart()
+            result = maps2(file, selected_risk, selected_season, selected_val)
+            return render_template("tab.html", weights=weights, selected_weight=selected_weight, states=states, selected_state=selected_state, selected_week=selected_week, week=week, min=40, max=52, seasons=seasons,
+                                   selected_risk=selected_risk, selected_season=selected_season, risk_factors=risk_factors,
+                                   result=result, chart=chart, result_cluster=result_cluster)
     # chart = sum_chart(selected_risk, selected_season)
     if selected_week < 13:
         selected_val = selected_week + 40
@@ -111,16 +151,13 @@ def home():
     result = maps1(file, selected_risk, selected_season, selected_val)
     # result = "C:\zoovision\static\default.png"
     chart = sum_chart()
-    return render_template("tab.html", states=states, selected_state=selected_state, week=week, min=40, max=52, seasons=seasons, selected_week=selected_week,
-                           selected_risk=selected_risk, selected_season=selected_season, viruses=viruses,
+    return render_template("tab.html", weights=weights, selected_weight=selected_weight, states=states, selected_state=selected_state, week=week, min=40, max=52, seasons=seasons, selected_week=selected_week,
+                           selected_risk=selected_risk, selected_season=selected_season,
                            risk_factors=risk_factors,
                            result=result, chart=chart)
-
-
 
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=5000, debug=True)
-
