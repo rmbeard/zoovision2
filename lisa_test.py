30e035e3d077165e3ccf5c9c3a0412d700b1ba6d
rmbeard <rachel.beard@asu.edu>
2019-05-16 12:24:08 -0700

    Update lisa_test.py

--- lisa_test.py
+++ lisa_test.py
@@ -4,7 +4,6 @@
 
 # %pip install matplotlib inline
 import matplotlib.pyplot as plt
-
 import pysal as ps
 from splot.esda import lisa_cluster, plot_moran, plot_local_autocorrelation, moran_scatterplot
 from esda.moran import Moran_Local
@@ -17,79 +16,29 @@ import geopandas as gpd
 # w = weights file, df= file with spatial data,
 # y = column to perform analysis on
 
-
 # First method will compute an inverse distance band weighting scheme
-def moran_inverse(file):
-    # Read in shapefile
-    df = file
-    # df = gpd.read_file("C:\zoovision\data\Region1.shp")
-    # print(df.dtypes)
 
-    y = df['ind_100t']
-    # Calculate weight
-    # First calculate minimum threshold distance to nearest neightbor
-    thresh = ps.min_threshold_dist_from_shapefile("C:\zoovision\data\Region1.shp")
+def moran_inverse_test(file, selected_risk, selected_val):
+    # Read in shapefile merged with csv
+    df = file
+    # print(df)
+    y = df[selected_risk]
+    # print(y)
+    thresh = ps.min_threshold_dist_from_shapefile("C:\zoovision\data\Export_Output.shp")
     print(thresh)
     # thresh = 1.1
     # weight based on fixed distance,(0 if not in threshold)
-    w = ps.weights.DistanceBand.from_shapefile("C:\zoovision\data\Region1.shp", threshold=thresh, binary=False)
+    w = ps.weights.DistanceBand.from_shapefile("C:\zoovision\data\Export_Output.shp", threshold=thresh, binary=False)
+    # print(w)
     # weight based on non fixed distance,(0 if not in threshold)
     # w = ps.weights.DistanceBand.from_shapefile("C:\zoovision\data\Region1.shp", binary=False)
 
     # transform r= standardize
-    moran_loc = ps.Moran_Local(y, w, transformation="r", permutations=999)
+    moran_loc = ps.Moran_Local(y, w, transformation="r", permutations=9999)
     print(moran_loc.p_sim)
-    fig, ax = lisa_cluster(moran_loc, df, p=0.05, figsize=(15, 10))
-
-    ax.set_title("Local Indicators of Spatial Association ",
-                 fontsize=35)  # plot_moran(moran_loc, zstandard=True, figsize=(10, 4))
-    # moran_scatterplot(moran_loc, ax=ax2)
-    # plot_local_autocorrelation(moran_loc, df, y, p=0.05, figsize=(18, 6))
-
-    # ax2.set_title("Moran scatterplot", fontsize=25)
-    # plt.show()
-    # lm = ps.Moran_Local(y, w, transformation = "r", permutations = 999)
-    # lm.q  # q denotes what type of cluster each location is, 1
-    # print(lm.q)
-    # sig = 1 * (lm.p_sim < 0.05)
-    # notsig = 1 * (sig * lm.q == 0)
-    # hotspot = 1 * (sig * lm.q == 1)
-    # coldspot = 3 * (sig * lm.q == 3)
-    # doughnut = 2 * (sig * lm.q == 2)
-    # diamond = 4 * (sig * lm.q == 4)
-    # print(tuple(hotspot))
-    # spots_labels = []
-    # spots = []
-    # if sig.any():
-    #     spots = sig
-    #     spots_labels = ['NA']
-    # if hotspot.any():
-    #     spots = spots + hotspot
-    #     spots_labels = spots_labels + ['Hotspot']
-    # if coldspot.any():
-    #     spots = spots + coldspot
-    #     spots_labels = spots_labels + ['Coldspot']
-    # if doughnut.any():
-    #     spots = spots + doughnut
-    #     spots_labels = spots_labels + ['Doughnit']
-    # if diamond.any():
-    #     spots = spots + diamond
-    #     spots_labels = spots_labels + ['Diamond']
-    #
-    # spots = hotspot + coldspot + doughnut + diamond
-    # spots_labels = ['0 na', '1 Hotspot',  '3 Coldspot', '2 Cold-Hot', '4 Hot-Cod']
-    # print(tuple(spots))
-    # print(tuple(spots_labels))
-    # labels = [spots_labels[i] for i in spots*1]
-    # # print(tuple(lm.p_z_sim))
-    # print(tuple(labels))
-    # hmap = colors.ListedColormap([ 'lightgrey', 'red', 'lightblue', 'blue', 'pink'])
-    # f, ax = plt.subplots(1, figsize=(9, 9))
-    # df.assign(cl=labels).plot(column='cl', categorical=True, \
-    #         k=2, cmap=hmap, linewidth=0.1, ax=ax, \
-    #         edgecolor='white', legend=True)
-    # ax.set_axis_off()
-    # plt.show()
+    fig, ax = lisa_cluster(moran_loc, df, p=0.05, figsize=(20, 15))
+    title = "Local Indicators of Spatial Association " + 'WEEK' + " " + str(selected_val)
+    ax.set_title(title, fontsize=35)  # plot_moran(moran_loc, zstandard=True, figsize=(10, 4))
 
 
 def moran_gen(file):
