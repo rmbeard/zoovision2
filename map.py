ddd803a4cd166fac36a9606a69b144ee7e8db173
rmbeard <rachel.beard@asu.edu>
2020-01-06 20:50:37 -0700

    Update map.py

--- map.py
+++ map.py
@@ -1,13 +1,11 @@
-# Written by Rachel Beard: last updated 10/30/19
+# Written by Rachel Beard: last updated 1/6/20
 # map.py contains several methods to assist in the rendering and saving of images to display in the zoovision webpage
-from lisa_test import moran_gen, moran_inverse_test
+# from lisa_test import moran_gen, moran_inverse_test
 import matplotlib.pyplot as plt
 from matplotlib import colors
 import geopandas as gpd
 import pandas as pd
-import numpy as np
 import os, time, glob
-# from shapely.geometry import Point, Polygon
 
 
 # method to display a map for a specific year, week, and strain
@@ -56,13 +54,14 @@ def maps1(shapefile, selected_risk, selected_strain, selected_season, selected_w
         os.mkdir('static')
     else:
         # Remove old plot files
+
         for filename in glob.glob(os.path.join('static', '*.png')):
             os.remove(filename)
     plotfile = os.path.join('static', str(time.time()) + '.png')
     plt.savefig(plotfile)
     # global tempMap
     # tempMap = plotfile
-    # plt.close()
+    plt.close()
     return plotfile
 
 
@@ -82,12 +81,12 @@ def maps2(shapefile, selected_strain, selected_risk, selected_season, selected_w
     ax.set_axis_off()
     rg1.plot(column=selected_risk, categorical=True, k=10, cmap='OrRd', linewidth=0.3, ax=ax,
                            edgecolor='white', legend=False)
-    cs[rg1[selected_strain] == 1].plot(ax=ax, markersize=20, color="blue", label="Pos Avian cases")
-    cs[rg1[selected_strain] == 2].plot(ax=ax, markersize=20, color="black", label="Pos Swine cases")
-    cs[rg1[selected_strain] == 3].plot(ax=ax, markersize=20, color="orange", label="Pos Avian and Swine cases")
-    cs[rg1[selected_strain] == 4].plot(ax=ax, markersize=20, color="yellow", label="Pos Human cases")
-    cs[rg1[selected_strain] == 5].plot(ax=ax, markersize=20, color="green", label="Pos Human and Swine cases")
-    cs[rg1[selected_strain] == 6].plot(ax=ax, markersize=20, color="red", label="Pos Human and Avian cases")
+    cs[rg1[selected_strain] == 1].plot(ax=ax, markersize=20, color="blue", label="Positive Avian cases")
+    cs[rg1[selected_strain] == 2].plot(ax=ax, markersize=20, color="black", label="Positive Swine cases")
+    cs[rg1[selected_strain] == 3].plot(ax=ax, markersize=20, color="orange", label="Positive Avian and Swine cases")
+    cs[rg1[selected_strain] == 4].plot(ax=ax, markersize=20, color="yellow", label="Positive Human cases")
+    cs[rg1[selected_strain] == 5].plot(ax=ax, markersize=20, color="green", label="Positive Human and Swine cases")
+    cs[rg1[selected_strain] == 6].plot(ax=ax, markersize=20, color="red", label="Positive Human and Avian cases")
     plt.legend(title=selected_strain + " Confirmed strain typing", loc='lower left', prop={'size': 16})
     vmin, vmax = 0, 0
     for i in df[selected_risk]:
@@ -101,39 +100,8 @@ def maps2(shapefile, selected_strain, selected_risk, selected_season, selected_w
     plt.savefig(plotfile)
     plotfile1 = plotfile
     # plt.show()
+    plt.close()
     return plotfile1
-
-
-def local_moran_test(shapefile, selected_risk, selected_season, selected_val, selected_weight, selected_week):
-    df = pd.read_csv('C:\zoovision\data\weeklydata1_test1.csv')
-    # shapefile
-    rg1 = gpd.read_file(shapefile)
-    df1 = df['SEASON'] == selected_season
-    df = df[df1]
-    df1 = df['WEEK'] == selected_val
-    df = df[df1]
-    df = df[['SEASON', "ILI POSITIVE", 'STATE_NAME', 'WEEK', 'PERCENT POSITIVE', '%UNWEIGHTED ILI']]
-    rg1 = rg1.merge(df, on='STATE_NAME')
-    # retrieve cluster classifications
-    # print(rg1)
-    print(selected_weight)
-    if selected_weight == 'Distance':
-        moran_inverse_test(rg1, selected_risk, selected_val)
-    else:
-        moran_gen(rg1)
-    if not os.path.isdir('static'):
-        os.mkdir('static')
-    else:
-        # Remove old plot files
-        for filename in glob.glob(os.path.join('static', '*.png')):
-            os.remove(filename)
-    # Use time since Jan 1, 1970 in filename in order make
-    # a unique filename that the browser has not chached
-    plotfile = os.path.join('static', str(time.time()) + '.png')
-    plt.savefig(plotfile)
-    return plotfile
-
-# fig, ax = plt.subplots(1, figsize=(15, 10))
 
 
 def precalc_moran(shapefile, selected_season, selected_val, selected_weight):
@@ -169,7 +137,7 @@ def precalc_moran(shapefile, selected_season, selected_val, selected_weight):
     hmap = colors.ListedColormap(['#d7191c', '#fdae61', '#abd9e9', '#2c7bb6', 'lightgrey'])
     rg1.assign(cl=labels).plot(column='cl', categorical=True, k=2, cmap=hmap, linewidth=0.3, ax=ax,
                                edgecolor='white', legend=True)
-    plt.legend(title='Cluster types', loc='lower left', prop={'size': 12})
+    plt.legend(title='Cluster types', loc='lower left', prop={'size': 15})
     # if not os.path.isdir('static'):
     #     os.mkdir('static')
     # else:
@@ -178,6 +146,7 @@ def precalc_moran(shapefile, selected_season, selected_val, selected_weight):
     #         os.remove(filename)
     plotfile = os.path.join('static', str(time.time()) + '.png')
     plt.savefig(plotfile)
+    plt.close()
     return plotfile
 
 
@@ -206,6 +175,7 @@ def sum_chart1(selected_week, selected_season, selected_state):
     plt.ylabel("%UNWEIGHTED ILI", fontsize=18)
     plotfile = os.path.join('static', str(time.time()) + '.png')
     plt.savefig(plotfile)
+    plt.close()
     return plotfile
 
 
